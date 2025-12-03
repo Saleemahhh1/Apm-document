6fee108cb2248484d2bce4be0e594d6c2f642ac5
Saleemahhh1 <202877436+Saleemahhh1@users.noreply.github.com>
2025-12-03 17:24:30 +0100

    Create roadmap.js

---
+++ pages/docs/roadmap.js
@@ -0,0 +1,22 @@
+import fs from 'fs'
+import path from 'path'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
+import Layout from '../../components/Layout'
+
+export default function Roadmap({ source }) {
+  return (
+    <Layout>
+      <div className="prose max-w-none">
+        <MDXRemote {...source} />
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'docs', 'roadmap.md')
+  const source = fs.readFileSync(filePath, 'utf8')
+  const mdxSource = await serialize(source)
+  return { props: { source: mdxSource } }
+}
